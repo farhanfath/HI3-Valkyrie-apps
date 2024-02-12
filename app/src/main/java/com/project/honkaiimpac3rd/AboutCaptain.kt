d8dd2775c2682084bd0ac1e990506e15fb409942
farhanfath <farhanfathur73@gmail.com>
2024-02-12 13:32:46 +0700

    adding about captain detail

--- app/src/main/java/com/project/honkaiimpac3rd/AboutCaptain.kt
+++ app/src/main/java/com/project/honkaiimpac3rd/AboutCaptain.kt
@@ -2,8 +2,12 @@ package com.project.honkaiimpac3rd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class AboutCaptain : AppCompatActivity() {
+
+    private lateinit var profieName : TextView
+    private lateinit var profileEmail : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_captain)
@@ -11,5 +15,12 @@ class AboutCaptain : AppCompatActivity() {
         val actionBar = supportActionBar
         actionBar!!.setDisplayHomeAsUpEnabled(true)
         actionBar!!.setTitle("About Captain")
+
+        profieName = findViewById(R.id.profileNama)
+        profileEmail = findViewById(R.id.profileEmail)
+
+        profieName.setText(R.string.profileName)
+        profileEmail.setText(R.string.profileEmail)
+
     }
 }
